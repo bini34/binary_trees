d75e58b453e36fa550d64e811f117e3fa8491975
bini34 <biniyamambachew@gmail.com>
2024-02-02 01:42:49 +0300

    update task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,9 +27,21 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if ((binary_tree_height(tree->left) - binary_tree_height(tree->right)) == 0)
-		return (1);
-	else
-		return (0);
+	int left_height = binary_tree_height(tree->left);
+    int right_height = binary_tree_height(tree->right);
+
+    if (left_height == right_height)
+    {
+        if (tree->left == NULL && tree->right == NULL)
+        {
+            return (1);
+        }
+        else if (tree->left != NULL && tree->right != NULL)
+        {
+            return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+        }
+    }
+
+    return (0);
 }
 
